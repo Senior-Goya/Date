be2eafacce1d330cd0f39b5e96cbf33b51435a98
Senior-Goya <jorge.rivas@mail.citytech.cuny.edu>
2020-07-08 12:37:54 -0400

    Added photos and member information

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { MemberListResolver } from './Resolvers/member-list.resolver';
+import { MemberDetailResolver } from './Resolvers/member-detail.resolver';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { UserService } from './_services/user.service';
 import { appRoutes } from './routes';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
@@ -13,10 +17,18 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
-import { MemberListComponent } from './member-list/member-list.component';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery-9';
+
+export function tokenGetter(){
+   return localStorage.getItem('token');
+}
 
 
 @NgModule({
@@ -27,19 +39,33 @@ import { RouterModule } from '@angular/router';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
+      NgxGalleryModule,
+      TabsModule.forRoot(),
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokenGetter,
+            whitelistedDomains: ['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      UserService,
+      ErrorInterceptorProvider,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,10 +1,14 @@
+import { User } from './_models/user';
+import { MemberDetailResolver } from './Resolvers/member-detail.resolver';
 import { AuthGuard } from "./_guards/auth.guard";
-import { MemberListComponent } from "./member-list/member-list.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
 import { Component } from "@angular/core";
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { ListsComponent } from "./lists/lists.component";
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListResolver } from './Resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,8 +17,8 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      {
-        path: 'members', component: MemberListComponent,},
+      {path: 'members', component: MemberListComponent, resolve : {users: MemberListResolver}},
+      {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent },
     ],
